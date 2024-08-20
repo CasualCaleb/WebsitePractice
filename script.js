3289e39e2c783a35c454f5e8f56e7f1b98e18b0a
CasualCaleb <45974833+CasualCaleb@users.noreply.github.com>
2024-08-19 22:28:24 -0700

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,14 @@ function goToMainPage() {
     window.location.href = "index.html";
 }
 
+function requestAPI() {
+    fetch('http://localhost:3000')
+    .then(response => response.text())
+    .then(data => {
+        alert(data);  // Display the server's response in an alert
+    })
+    .catch(error => {
+        alert("Error connecting to API")
+    });
+}
 console.log("JS Connected");

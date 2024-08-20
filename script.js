cdd00e75a080048fd06876db7ee53b4502615b1d
CasualCaleb <45974833+CasualCaleb@users.noreply.github.com>
2024-08-19 19:56:09 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+function MyFunction() {
+    alert("The button was pressed!");
+}
+
+function goToResidentsPage() {
+    window.location.href = "residents.html";
+}
+
+function goToMainPage() {
+    window.location.href = "index.html";
+}
+
+console.log("JS Connected");
